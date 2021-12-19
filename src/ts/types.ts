7f11316f7116545e39626cd2d43535aa5df0cdd6
Cameron Matsui <cmatsui11@gmail.com>
2021-12-18 22:39:19 -0600

    start refactor

---
+++ src/ts/types.ts
@@ -0,0 +1,41 @@
+//================================================================================
+/**
+ * @file types.ts
+ * @author Cameron Matsui (cmatsui22@amherst.edu)
+ * @date December 2021.
+ */
+//================================================================================
+
+
+
+//================================================================================
+// TYPES
+//================================================================================
+
+// A type representing the color and transparency of a pixel.
+export type RGBA = {
+    r: number;
+    g: number;
+    b: number;
+    a: number;
+}
+
+// A type representing a 2d coordinate position.
+export type Coordinate = {
+    x: number;
+    y: number;
+}
+
+// A type representing the parameters of an affine transform.
+export type ParameterizedAffineTransform = {
+    r: number;
+    s: number;
+    thetaD: number; // in degrees
+    phiD: number; // also in degrees
+    e: number;
+    f: number;
+}
+
+//================================================================================
+// END TYPES
+//================================================================================
